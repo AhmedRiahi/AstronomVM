5e0e638d3623b198a036f894895dadda707d5277
Ahmed RIAHI <contact@ahmedriahi.net>
2018-09-18 08:59:33 +0100

    adding row filter component module

---
+++ RowFilterComponent/src/main/java/com/astronomvm/rowFilter/RowFilterComponent.java
@@ -0,0 +1,21 @@
+package com.astronomvm.rowFilter;
+
+import com.astronomvm.component.BaseComponent;
+import com.astronomvm.component.exception.ComponentException;
+import com.astronomvm.core.data.output.ResultSet;
+import com.astronomvm.core.meta.ComponentMeta;
+
+import java.util.Optional;
+
+public class RowFilterComponent extends BaseComponent {
+    
+    @Override
+    public ComponentMeta getComponentMeta() {
+        return null;
+    }
+
+    @Override
+    public Optional<ResultSet> execute() throws ComponentException {
+        return null;
+    }
+}
