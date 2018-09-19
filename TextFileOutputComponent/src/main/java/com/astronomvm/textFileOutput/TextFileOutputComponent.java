67667f0e9a63aade78df194062ddd82d6838d0af
Ahmed RIAHI <contact@ahmedriahi.net>
2018-09-19 13:27:15 +0100

    module creation

---
+++ TextFileOutputComponent/src/main/java/com/astronomvm/textFileOutput/TextFileOutputComponent.java
@@ -0,0 +1,19 @@
+package com.astronomvm.textFileOutput;
+
+import com.astronomvm.component.BaseComponent;
+import com.astronomvm.component.exception.ComponentException;
+import com.astronomvm.core.data.output.ResultFlow;
+import com.astronomvm.core.meta.ComponentMeta;
+
+public class TextFileOutputComponent extends BaseComponent {
+
+    @Override
+    public ComponentMeta getComponentMeta() {
+        return null;
+    }
+
+    @Override
+    public ResultFlow execute() throws ComponentException {
+        return null;
+    }
+}
