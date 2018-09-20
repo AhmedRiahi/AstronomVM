284715b936fa507f7895fe98a631f77d0779cbaa
Ahmed RIAHI <contact@ahmedriahi.net>
2018-09-20 13:04:13 +0100

    remove db config

--- AstonomDesigner/src/main/java/com/astronomvm/designer/AstronomDesignerApplication.java
+++ AstonomDesigner/src/main/java/com/astronomvm/designer/AstronomDesignerApplication.java
@@ -1,11 +1,15 @@
 package com.astronomvm.designer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 public class AstronomDesignerApplication {
 
     public static void main(String[] args){
