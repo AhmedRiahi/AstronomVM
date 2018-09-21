d8b82cc68463d842aa42ce767ef976014d019223
Ahmed RIAHI <contact@ahmedriahi.net>
2018-09-21 21:30:54 +0200

    enable swagger

--- AstonomDesigner/src/main/java/com/astronomvm/designer/AstronomDesignerApplication.java
+++ AstonomDesigner/src/main/java/com/astronomvm/designer/AstronomDesignerApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @ComponentScan
 @SpringBootApplication
+@EnableSwagger2
 public class AstronomDesignerApplication {
 
     public static void main(String[] args){
