2bdaa3d0f0bff76b90d7e8e3a1f3ee8e93f048ea
Ahmed RIAHI <contact@ahmedriahi.net>
2018-09-24 08:22:37 +0100

    adding astronom agent module

---
+++ AstronomAgent/src/main/java/com/astronomvm/agent/AstronomMetaFlowParser.java
@@ -0,0 +1,12 @@
+package com.astronomvm.agent;
+
+import com.astronomvm.core.meta.AstronomMetaFlow;
+
+public class AstronomMetaFlowParser {
+
+    private AstronomMetaFlowParser(){}
+
+    public static AstronomMetaFlow parseMetaFlow(String fileContent){
+        return null;
+    }
+}
